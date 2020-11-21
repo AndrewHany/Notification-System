43704222d0dc40f08f72b41877a4ce5ca7b96e7b
Andrew Hany <ahany@raisaenergy.com>
2020-11-22 01:02:18 +0200

    [FEAT] Adds unit testing base structure

--- src/server.js
+++ src/server.js
@@ -1,31 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const db = require("./models");
-
-const app = express();
-require("./routes/notification.routes")(app);
-
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the root of the application." });
-});
-
+const app = require("./app");
 // set port, listen for requests
 const PORT = 8081;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
